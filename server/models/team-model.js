c1a2b10d8d96db9af94478e89b11b354d776381c
Syke2good <akashbhandari930@gmail.com>
2022-01-28 12:09:19 -0800

    added a few codes

---
+++ server/models/team-model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Team = new Schema(
+    {
+        name: { type: String, required: true },
+        leauge: { type: [String], required: true },
+        rating: { type: Number, required: true },
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('teams', Team)
