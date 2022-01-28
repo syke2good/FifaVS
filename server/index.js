5f43f5453696256813963c1d9da7f657ee4dc0d1
Syke2good <akashbhandari930@gmail.com>
2022-01-28 12:13:40 -0800

    testing

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const db = require('./db')
+const teamRouter = require('./routes/team-router')
 
 const app = express()
 const apiPort = 3000
@@ -16,5 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', teamRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
---
+++ server/routes/team-router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const TeamCtrl = require('../controllers/team-ctrl')
+
+const router = express.Router()
+
+router.post('/team', TeamCtrl.createTeam)
+router.put('/team/:id', TeamCtrl.updateTeam)
+router.delete('/team/:id', TeamCtrl.deleteTeam)
+router.get('/team/:id', TeamCtrl.getTeamById)
+router.get('/teams', TeamCtrl.getTeams)
+
+module.exports = router
