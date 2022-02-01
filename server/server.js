aa44f0dd63f7c3f0d805abf629dafa4a4ff7177b
Syke2good <akashbhandari930@gmail.com>
2022-02-01 11:27:19 -0800

    fixed few codes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const db = require('./config/connection')
+const db = require('./db')
 const teamRouter = require('./routes/team-router')
 
 const app = express()
