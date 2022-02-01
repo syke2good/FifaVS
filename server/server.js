5802e2efd1e25b1a70d5caa00889c2aea7a39c7d
Syke2good <akashbhandari930@gmail.com>
2022-01-31 19:51:49 -0800

    trying

---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const db = require('./db')
+const teamRouter = require('./routes/team-router')
+
+const app = express()
+const apiPort = 3002
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use('/api', teamRouter)
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
+/*const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});*/
