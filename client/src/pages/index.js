dfd4fb09487f383eb69e2005a4af1e82280a9d50
Kushal Khattri <kushalk62@gmail.com>
2022-02-01 21:13:44 -0800

    created random number generator

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
 import TeamList from './TeamsList'
 import TeamInsert from './TeamsInsert'
 import TeamUpdate from './TeamsUpdate'
+import TeamVs from './TeamVs'
 
-export { TeamList, TeamInsert, TeamUpdate }
+export { TeamList, TeamInsert, TeamUpdate, TeamVs }
